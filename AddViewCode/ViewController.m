89ed3756ce776a631c4ccb17b0313a263f3f001e
noprom <tyee.noprom@qq.com>
2015-07-01 20:46:31 +0800

    add img using code

--- AddViewCode/ViewController.m
+++ AddViewCode/ViewController.m
@@ -14,9 +14,25 @@
 
 @implementation ViewController
 
+/**
+ *  控制器创建后，接着创建控制器里面的view
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // 1.动态创建自己的按钮
+    UIButton *button = [[UIButton alloc]init];
+    // 2.给按钮设置文字
+    [button setTitle:@"你好啊" forState:UIControlStateNormal];//(默认状态)
+    [button setTitle:@"点我干嘛" forState:UIControlStateHighlighted]; // 高亮状态
+    UIImage *imgNormal = [UIImage imageNamed:@"btn_01"]; //默认状态
+    UIImage *imgHighLighted = [UIImage imageNamed:@"btn_02"];// 高亮状态
+    
+    // 3.设置背景图
+    [button setBackgroundImage:imgNormal forState:UIControlStateNormal];//默认状态
+    [button setBackgroundImage:imgHighLighted forState:UIControlStateHighlighted];//高亮状态
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
